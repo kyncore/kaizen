61b7dced9d9e93a28a203ab47e3c0dea8de3e2fa
kyncore <yen-ning@coremobile.co.jp>
2026-01-05 18:52:45 +0900

    open 1.1-dev

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,6 +1,5 @@
 <?php
 declare(strict_types=1);
-
 use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
@@ -10,12 +9,17 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesRele
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
-use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (MBConfig $mbConfig): void {
-    // ✅ important: match your actual main branch
     $mbConfig->defaultBranch('main');
+    $mbConfig->packageDirectories([
+        __DIR__ . '/packages/core',
+        __DIR__ . '/packages/survey',
+        __DIR__ . '/packages/aws_agent',
+    ]);
 
+
+    // release workers - in order to execute
     $mbConfig->workers([
         UpdateReplaceReleaseWorker::class,
         SetCurrentMutualDependenciesReleaseWorker::class,
