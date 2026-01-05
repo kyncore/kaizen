0477d40d5870690109151d69f02c02da8502416c
kyncore <yen-ning@coremobile.co.jp>
2026-01-05 17:36:17 +0900

    open 1.1-dev

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
@@ -11,6 +12,12 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 
 return static function (MBConfig $mbConfig): void {
     $mbConfig->defaultBranch('main');
+    $mbConfig->packageDirectories([
+        __DIR__ . '/packages/core',
+        __DIR__ . '/packages/survey',
+        __DIR__ . '/packages/aws_agent',
+    ]);
+
     // release workers - in order to execute
     $mbConfig->workers([
         UpdateReplaceReleaseWorker::class,
